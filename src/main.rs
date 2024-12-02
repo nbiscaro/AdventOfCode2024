426619a0f9382216ecdbb608062d61c3ebaeef47
nbiscaro <biscaronolan@gmail.com>
2024-12-02 17:18:34 -0500

    use util in main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+mod utils;
+
+use utils::puzzle_input::read_file_lines;
+
 fn main() {
-    println!("Hello, world!");
+    let path = "input.txt";
+
+    // Read the file and store the lines
+    let _lines = match read_file_lines(path) {
+        Ok(lines) => lines,
+        Err(err) => {
+            eprintln!("Error reading input file: {}", err);
+            return;
+        }
+    };
 }
