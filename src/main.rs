1d11684d125f8ebbb2207582e31eaf7c16db24e1
nbiscaro <biscaronolan@gmail.com>
2024-12-07 16:23:22 -0500

    part 2 solution

--- src/main.rs
+++ src/main.rs
@@ -14,11 +14,11 @@ fn main() {
         }
     };
 
-    fnSolvePart1(&lines);
-    fnSolvePart2(&lines);
+    solve_part_1(&lines);
+    solve_part_2(&lines);
 }
 
-fn solvePart1(input: &Vec<String>) {
+fn solve_part_1(input: &Vec<String>) {
     // Your solution goes here
 
     // Given two columns of numbers sum the minimum pairwise we need to:
@@ -29,14 +29,12 @@ fn solvePart1(input: &Vec<String>) {
     let mut list_2: Vec<i32> = Vec::new();
 
     for line in input {
-        let words: Vec<&str> = line.split_whitespace().collect();
+        let mut words = line.split_whitespace();
+        let word_1 = words.next().unwrap_or_default();
+        let word_2 = words.next().unwrap_or_default();
 
-        if let [word_1, word_2] = &words[..] {
-            list_1.push((*word_1).parse::<i32>().expect("Failed to parse word1"));
-            list_2.push((*word_2).parse::<i32>().expect("Failed to parse word2"));
-        } else {
-            eprintln!("Unexpected input format");
-        }
+        list_1.push(word_1.parse().expect("Failed to parse word1"));
+        list_2.push(word_2.parse().expect("Failed to parse word2"))
     }
 
     list_1.sort();
@@ -51,6 +49,32 @@ fn solvePart1(input: &Vec<String>) {
     println!("Part 1 answer: {}", edit_distance);
 }
 
-fn solvePart2(input &Vec <String>) {
-    // Your part 2 solutuon goes here
+fn solve_part_2(input: &Vec<String>) {
+    // Create map for right list of the form {number : frequency}
+    // For each number in the left list multiply by frequency for that number in the right list
+
+    use std::collections::HashMap;
+
+    let mut left_list: Vec<i32> = Vec::new();
+    let mut right_map: HashMap<i32, i32> = HashMap::new();
+
+    for line in input {
+        let mut row = line.split_whitespace();
+
+        let word_1 = row.next().unwrap_or_default();
+        let word_2 = row.next().unwrap_or_default();
+
+        let left_num: i32 = word_1.parse().expect("Failed to parse word1");
+        let right_num: i32 = word_2.parse().expect("Failed to parse word2");
+
+        *right_map.entry(right_num).or_insert(0) += 1;
+        left_list.push(left_num);
+    }
+
+    let mut similarity_score: i32 = 0;
+    for left_num in left_list {
+        similarity_score += left_num * right_map.get(&left_num).unwrap_or(&0);
+    }
+
+    println!("Part 2 answer: {}", similarity_score);
 }
